07403dcd3c89402bd7ca268184c42b0a4bd6415a
Sebastien-PILUSO <63786997+Sebastien-PILUSO@users.noreply.github.com>
2024-12-11 14:59:45 +0100

    remove BBP statement

--- bbp_deformation/bbp_ants_nonlinear_alignment.py
+++ bbp_deformation/bbp_ants_nonlinear_alignment.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python
 # -*- coding: latin-1 -*-
 
-# Funding & Acknowledgment
-"""The development of this software was supported by funding to the Blue Brain Project, a research center of the École polytechnique fédérale de Lausanne (EPFL), from the Swiss government's ETH Board of the Swiss Federal Institutes of Technology.
-Copyright (c) 2024 Blue Brain Project/EPFL
-"""
-
 
 # =============================================================================================
 # Librairies importation
