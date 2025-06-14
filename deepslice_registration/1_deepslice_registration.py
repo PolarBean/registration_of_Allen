71b9bab501d284508a62e75fb9b24102e6e674ed
polarbean <harry.carey95@gmail.com>
2025-06-14 21:54:51 +0200

    use all images for predictions

--- deepslice_registration/1_deepslice_registration.py
+++ deepslice_registration/1_deepslice_registration.py
@@ -5,18 +5,19 @@ import numpy as np
 
 Model = DeepSlice.DSModel("mouse")
 
-path = r"/home/harryc/github/AllenDownload/downloaded_data/*/*/10um"
+path = r"/mnt/g/AllenDataalignmentProj/resolutionPixelSizeMetadata/ISH/*"
 folders = glob(path)
-
-for folder in folders[-4:-3]:
-    image_names = glob(folder + "/*")
+specimens = np.unique([f.split('/')[-1] for f in folders])
+template = "/mnt/g/AllenDataalignmentProj/resolutionPixelSizeMetadata/ISH/{}/*/10um_new/*.jpg"
+for specimen in specimens:
+    image_names = glob(template.format(specimen))
     Model.predict(
         image_directory=None,
         image_list=image_names,
         ensemble=False,
         use_secondary_model=True,
     )
-    image_folders = [i.split("/")[-3] for i in image_names]
+    image_folders = np.unique([i.split("/")[-3] for i in image_names])
     Model.predictions["Filenames"] = [
         f"{fol}/10um/{nam}"
         for fol, nam, in zip(image_folders, Model.predictions["Filenames"])
@@ -30,13 +31,9 @@ for folder in folders[-4:-3]:
         section_thickness = 35
     else:
         section_thickness = 25
-    if "reference-2" in folder:
+    if "reference-2" == specimen:
         section_thickness *= -1
 
     section_thickness = np.round(section_thickness)
     Model.enforce_index_spacing(section_thickness)
-    specimen = folder.split("/")[-3]
-    out_path = "/".join(folder.split("/")[:-2]) + "/" + specimen 
-
-    Model.save_predictions(out_path)
     Model.save_predictions(f"registrations/{specimen}")
